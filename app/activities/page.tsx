08f9df6ea5793e4302dee6ec6acabe41052ae3fa
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2025-01-26 14:20:56 +0900

    add figure to activity

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import { useState } from 'react'
+import Image from 'next/image' 
 
 interface Activity {
   date: string
   title: string
   description: string
+  figure: string
   blogLink?: string
 }
 
@@ -18,6 +20,7 @@ const ActivityItem: React.FC<Activity> = ({ date, title, description, blogLink }
       </div>
       <div className="ml-8">
         <h3 className="text-xl font-semibold mb-2">{title}</h3>
+        <Image src="/2023-07-02-himawari/himawari-1.jpg" alt="Sample Image" width={500} height={500} objectFit="contain" />
         <p className="text-gray-700 mb-2">{description}</p>
         {blogLink && (
           <a 
@@ -38,46 +41,9 @@ const ActivityTimeline: React.FC = () => {
   const [visibleActivities, setVisibleActivities] = useState(5)
   const activities: Activity[] = [
     {
-      date: '2023年12月15日',
-      title: '冬季将棋大会開催',
-      description: '地域の方々を招いて冬季将棋大会を開催しました。参加者は100名を超え、大盛況でした。',
-      blogLink: '/blog/winter-tournament-2023'
-    },
-    {
-      date: '2023年10月5日',
-      title: '小学校での将棋教室',
-      description: '地元の小学校で将棋教室を開催。子どもたちの興味を引き出すことができました。',
-      blogLink: '/blog/elementary-school-class'
-    },
-    {
-      date: '2023年8月20日',
-      title: '夏季合宿',
-      description: 'メンバーで2泊3日の合宿を行い、技術向上と親睦を深めました。',
-      blogLink: '/blog/summer-camp-2023'
-    },
-    {
-      date: '2023年6月30日',
-      title: 'オンライン将棋大会',
-      description: '初のオンライン将棋大会を開催。全国から200名以上の参加がありました。',
-      blogLink: '/blog/online-tournament-2023'
-    },
-    {
-      date: '2023年4月15日',
-      title: '新入生歓迎会',
-      description: '新しく加入した学生メンバーを歓迎する会を開催しました。',
-      blogLink: '/blog/welcome-party-2023'
-    },
-    {
-      date: '2023年2月11日',
-      title: '将棋フェスティバル参加',
-      description: '市内で開催された将棋フェスティバルにブース出展しました。',
-      blogLink: '/blog/shogi-festival-2023'
-    },
-    {
-      date: '2023年1月5日',
-      title: '新年将棋大会',
-      description: '新年を祝して、地域の方々と共に新年将棋大会を開催しました。',
-      blogLink: '/blog/new-year-tournament-2023'
+      date: '2023年7月2日',
+      title: '向日葵将棋大会開催',
+      description: '地域の方々を招いて冬季将棋大会を開催しました。参加者は100名を超え、大盛況でした。'
     }
   ]
 
