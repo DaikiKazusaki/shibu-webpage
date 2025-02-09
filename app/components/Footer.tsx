0bcbc71d7bd1446824ab43b73325f34e35396aa8
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2025-02-09 17:01:19 +0900

    fix the context

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-gray-800 text-white py-6 mt-auto">
       <div className="container mx-auto text-center">
         <p>&copy; 2025 江坂学生支部. All rights reserved.</p>
-        <p>This website is made by a group of students.</p>
+        <p>This website is created by a group of students.</p>
       </div>
     </footer>
   )
