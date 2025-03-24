d11c956f1f503ee21d2dbcdc57a1c6016de5b308
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2025-03-24 14:43:28 +0900

    fix the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,10 @@ export default function RootLayout({
   return (
     <html lang="ja" className="h-full">
       <body className={`${inter.className} flex flex-col min-h-full`}>
-        <div className="fixed top-0 left-0 right-0 z-50">
+        <div className="top-0 left-0 right-0 z-50">
           <Header />
         </div>
-        <div className="flex-grow flex flex-col sm:flex-row overflow-hidden pt-[88px]">
+        <div className="flex-grow flex flex-col sm:flex-row overflow-hidden">
           <div className="hidden xl:block min-w-[120px] max-w-[320px] w-[14%] flex-shrink-0">
             {isActivitiesPage && <LeftSidebar />}
           </div>
